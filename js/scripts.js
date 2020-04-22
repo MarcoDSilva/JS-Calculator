0da0d7f25d7134f8b7adcdeaebd3e80d09d0b732
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-04-22 21:04:23 +0100

    initial basic design and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
 //js
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach(btn => btn.addEventListener('click', clicked));
+
+function clicked() {
+    console.log(this.name)
+}
+
+
+function add(arr) {
+
+}
+
+function multiply(arr) {
+
+}
+
+function division(arr) {
+
+}
+
+function subtract(arr) {
+
+}
+
+function operate(operator, num1, num2) {
+
+}
+
