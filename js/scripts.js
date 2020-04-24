7b7a9c0e0feee12318c669816633e9920a450992
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-04-24 20:18:15 +0100

    starting to refactor and planning the functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,81 +6,90 @@ buttons.forEach(btn => btn.addEventListener('click', clicked));
 
 let numIntroduced = "";
 let arrOperations = [];
-let keysPressed = [];
 let actualOperation = "";
 
 //need to verify the number of operation clicks before giving result or update right away
 //get the last result if number is not given
-//refactor the lens of arrs
+//refactor the buttons clicked, X function for numbers, X function for operators
+
 function clicked() {
     console.log(this.name)
 
     if (!isNaN(+this.name)) {
         numIntroduced += this.name;
         showCalculations();
-    } else if (this.name !== "=" && isNaN(+this.name)) {
+    }
+    else if (this.name !== "=" && isNaN(+this.name)) {
         actualOperation = this.name;
 
         if (arrOperations.length === 0) {
-            arrOperations.push(+numIntroduced);
-            numIntroduced = "";
-        } else {
+            addNumberToArr(numIntroduced);
+        }
+        else {
             if (numIntroduced == "") {
-                arrOperations.push(arrOperations[arrOperations.length - 1]);
-            } else {
+                addNumberToArr(arrOperations[arrOperations.length - 1]);
+            }
+            else {
                 arrOperations.push(+numIntroduced);
             }
         }
-    } else if (this.name === "=") {
-        arrOperations.push(+numIntroduced);
-        numIntroduced = ""; 
-        console.log(operate(actualOperation, arrOperations[arrOperations.length - 2],
-            arrOperations[arrOperations.length - 1]));
-        showResult();
     }
-    
+    else if (this.name === "=") {
+        equals();
+    }
 }
 
-function add(num1,num2) {
+function add(num1, num2) {
     arrOperations.push(num1 + num2);
-    return arrOperations[arrOperations.length - 1];
 }
 
-function multiply(num1,num2) {
+function multiply(num1, num2) {
     arrOperations.push(num1 * num2);
-    return arrOperations[arrOperations.length - 1];
 }
 
-function division(num1,num2) {
+function division(num1, num2) {
     arrOperations.push(num1 / num2);
-    return arrOperations[arrOperations.length - 1];
 }
 
-function subtract(num1,num2) {
+function subtract(num1, num2) {
     arrOperations.push(num1 - num2);
-    return arrOperations[arrOperations.length - 1];
+}
+
+function equals() {
+    arrOperations.push(+numIntroduced);
+    numIntroduced = "";
+
+    operate(actualOperation, arrOperations[arrOperations.length - 2],
+        arrOperations[arrOperations.length - 1]);
+
+    showResult()
 }
 
 function operate(operator, num1, num2) {
-    const result = arrOperations[arrOperations.length - 1];
-    switch (operator) {      
+    switch (operator) {
         case "*":
-            return multiply(num1,num2);
+            return multiply(num1, num2);
         case "/":
-            return division(num1,num2);           
+            return division(num1, num2);
         case "+":
-            return add(num1,num2);
+            return add(num1, num2);
         case "-":
-           return subtract(num1,num2);
+            return subtract(num1, num2);
         default:
             break;
     }
 }
 
 function showCalculations() {
-    visor.innerHTML = +numIntroduced;
+    visor.innerHTML = numIntroduced;
 }
 
 function showResult() {
+    console.log(arrOperations[arrOperations.length - 1]);
     visor.innerHTML = arrOperations[arrOperations.length - 1];
+}
+
+function addNumberToArr(num) {
+    arrOperations.push(+num);
+    numIntroduced = "";
 }
